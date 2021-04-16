f231fd784fbcd0cafae705515697b1cce51eebbc
sohyun934 <a6570407@naver.com>
2021-04-16 11:00:00 +0900

    브루트포스_약수 구하기

---
+++ P2501.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+nums = []
+
+for i in range(1, n+1):
+  if n % i == 0:
+    nums.append(i)
+
+if len(nums) < k:
+  print(0)
+else:
+  nums.sort()
+  print(nums[k-1])
